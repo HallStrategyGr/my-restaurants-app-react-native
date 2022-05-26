7cec665c81cf91a5b5225a3d03645c85982ff59f
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-26 11:18:52 -0400

    added styling-addresses-cuisine

--- App.js
+++ App.js
@@ -1,74 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import {
-  StyleSheet,
-  Text,
-  View,
-  ActivityIndicator,
-  ImageBackground,
-  ScrollView
-} from "react-native";
+  SafeAreaView, View, ActivityIndicator, ImageBackground, ScrollView
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import RestaurantCard from './src/components/RestaurantCard';
+import styles from './src/styles';
 
-const image = {
-  uri: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI=",
-};
+const bgImage = { uri: 'https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg' };
 
 export default function App() {
   const [allRestaurants, setAllRestaurants] = useState();
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(
-          "https://my-first-firestore-st.web.app/restaurants/"
-        );
-        const data = await response.json();
-        setAllRestaurants(data);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getData();
-  }, []);
+    fetch('https://my-first-firestore-bc.web.app/restaurants/')
+      .then(response => response.json())
+      .then(setAllRestaurants)
+      .catch(console.error)
+  }, [])
 
   return (
+    <NavigationContainer>
     <View style={styles.container}>
-      <ImageBackground
-        resizeMode="cover"
-        source={image}
-        style={styles.container}
-      >
-        <ScrollView>
-          {allRestaurants ? (
-            allRestaurants?.map((singleRest) => (
-              <Text style={styles.text} key={singleRest.id}>
-                {singleRest.name}{" "}
-              </Text>
-            ))
-          ) : (
-            <ActivityIndicator size="large" color="orange" />
-          )}
-          <Text> Hello, there!</Text>
-          <StatusBar style="auto" />
-        </ScrollView>
+      <ImageBackground resizeMode='cover' source={bgImage} style={styles.container}>
+        <SafeAreaView style={styles.container}>
+          <ScrollView>
+            {!allRestaurants
+              ? <ActivityIndicator size='large' color='orange' />
+              :
+              allRestaurants.map(singleRest => (
+                <RestaurantCard key={singleRest.id} singleRest={singleRest} />
+              ))
+            }
+          </ScrollView>
+        </SafeAreaView>
+        <StatusBar style="auto" />
       </ImageBackground>
     </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 34,
-    marginVertical: 120,
-    color: "white",
-    backgroundColor: "black",
-  },
-});
---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,20 @@
+import { View, Text, Image } from 'react-native';
+import styles from '../styles/index';
+
+// name, cuisine, address, rating
+
+export default function RestaurantCard({ singleRest }) {
+  return (
+    <View style={styles.restaurantCard}>
+      <Text style={styles.restaurantsName}>
+        {singleRest.name}
+      </Text>
+      <Text style={styles.cuisine}>
+        {singleRest.cuisine}
+      </Text>
+      <Text style={styles.address}>
+        {singleRest.address}
+      </Text>
+      <Image source={{ uri: singleRest.image }} style={{ width: '100%', height: 100 }} />
+    </View>
+  )}
