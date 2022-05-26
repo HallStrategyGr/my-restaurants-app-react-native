65f3f840c969391d83bfee97687eb473b7c1f4f5
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-26 15:29:33 -0400

    added individual restaurant cards

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import {SafeAreaView, View, ImageBackground} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/scenes/Home';
-import Details from './src/scenes/Details';
+import styles from './src/styles';
+
+const bgImage = { uri: 'https://png.pngtree.com/background/20210709/original/pngtree-food-western-food-steak-tomato-picture-image_941801.jpg' };
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={Home} options={{title:'Local Restaurants'}}/>
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <ImageBackground resizeMode='cover' source={bgImage} style={styles.container}>
+        <SafeAreaView style={styles.container}>
+          <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen name="Home" component={Home}/>
+          </Stack.Navigator>
+        </SafeAreaView>
+        <StatusBar style="auto" />
+      </ImageBackground>
+    </View>
     </NavigationContainer>
   );
 }
