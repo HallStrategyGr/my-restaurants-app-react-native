40597653817fd91e1a87c9f7972a7fd8ca840612
Alexandra M. Hall <hallstrategygroup@gmail.com>
2022-05-26 09:05:48 -0400

    my-restaurant-app

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  ImageBackground,
+  ScrollView
+} from "react-native";
+
+const image = {
+  uri: "https://media.istockphoto.com/photos/table-top-view-of-spicy-food-picture-id1316145932?b=1&k=20&m=1316145932&s=170667a&w=0&h=feyrNSTglzksHoEDSsnrG47UoY_XX4PtayUPpSMunQI=",
+};
 
 export default function App() {
+  const [allRestaurants, setAllRestaurants] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          "https://my-first-firestore-st.web.app/restaurants/"
+        );
+        const data = await response.json();
+        setAllRestaurants(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        resizeMode="cover"
+        source={image}
+        style={styles.container}
+      >
+        <ScrollView>
+          {allRestaurants ? (
+            allRestaurants?.map((singleRest) => (
+              <Text style={styles.text} key={singleRest.id}>
+                {singleRest.name}{" "}
+              </Text>
+            ))
+          ) : (
+            <ActivityIndicator size="large" color="orange" />
+          )}
+          <Text> Hello, there!</Text>
+          <StatusBar style="auto" />
+        </ScrollView>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +60,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 34,
+    marginVertical: 120,
+    color: "white",
+    backgroundColor: "black",
   },
 });
